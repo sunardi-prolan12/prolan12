16f61d8813f0a955d0ddd6cdd8bbc34c03b43e69
sunardi-prolan12 <pouwyan.ss@gmail.com>
2017-05-31 01:24:14 +0700

    Add files via upload

---
+++ Minggu10/Karcis.java
@@ -0,0 +1,19 @@
+package sunardigeneric;
+public class Karcis {
+
+    public static void main(String[] args) {
+        
+        
+        parkir<String> p2 = new parkir<String>("12:77:12");
+        String z = p2.getJam();
+        System.out.println("Jam Masuk = " + z);
+        
+        
+  
+        parkir p3 = new parkir<String>("21:12:13");
+        String d = (String) p3.getJam();
+        System.out.println("Jam Keluar = " + d);
+        
+    }
+    
+}
