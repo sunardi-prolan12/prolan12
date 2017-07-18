6896b76490d13422e848ee512d914e28f2855e27
sunardi-prolan12 <pouwyan.ss@gmail.com>
2017-07-18 12:26:22 +0700

    Add files via upload

---
+++ sunardigeneric/parkir.java
@@ -0,0 +1,24 @@
+package Minggu10;
+
+
+class parkir<T>{
+    private T jam;
+
+    public parkir(T jam) {
+
+        this.jam = jam;
+
+    }
+
+    public void setJam(T jam) {
+        this.jam = jam;
+    }
+
+    public T getJam() {
+        return jam;
+    }
+
+    public void getType() {
+        System.out.println("Tipe awalnya " + jam.getClass().getName());
+    }
+}
